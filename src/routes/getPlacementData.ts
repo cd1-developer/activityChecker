11beeb6c6c8b31560cdd8d0bebcebcb095813739
cd1 <cd1@socialthreesixty.co>
2025-01-06 06:27:27 +0000

    Get Placement Data is ready

--- src/routes/getPlacementData.ts
+++ src/routes/getPlacementData.ts
@@ -3,33 +3,38 @@ import passwordModel from "../model/passwordModel";
 
 const router = Router();
 
-router.get("/",async(req:Request,res:Response)=>{
-    try{
-        const {deviceId} = req.headers
-        
-        // If deviceId is found then return the Device Data
-        if(deviceId){
-            let device = await passwordModel.findOne({deviceId});
-            if(device){
-                return res.json({success:true,device})
-            }
-            // if Devices are not found then give me all free Placements
-            let freeDevices = await passwordModel.find({isfree:true});
-                if(freeDevices.length > 0){
-                    return res.json({ success: true, freeDevice: freeDevices });
-                }else{
-                    return res.json({ success: false, message:"No free devices available"});
-                }
-            
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const { deviceId } = req.query;
 
-            
-        }else{
-            return res.json({success:false,message:"Device Id not found"})
-        }
-    }catch(e:any){
-        console.log(`Error in gettng Free Placement ${e.message}`);
-        return res.json({success:false,message:"Error in getting Free Placement"})
+    // If deviceId is found then return the Device Data
+    if (deviceId) {
+      let device = await passwordModel.findOne({ deviceId });
+      if (device) {
+        return res.json({ success: true, device });
+      }
+      // if Devices are not found then give me all free Placements
+      let freeDevices = await passwordModel
+        .find({ isfree: true })
+        .select("-password -twoFactorCode");
+      if (freeDevices.length > 0) {
+        return res.json({ success: true, freeDevice: freeDevices });
+      } else {
+        return res.json({
+          success: false,
+          message: "No free devices available",
+        });
+      }
+    } else {
+      return res.json({ success: false, message: "Device Id not found" });
     }
-})
+  } catch (e: any) {
+    console.log(`Error in gettng Free Placement ${e.message}`);
+    return res.json({
+      success: false,
+      message: "Error in getting Free Placement",
+    });
+  }
+});
 
-export default router
+export default router;
