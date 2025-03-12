904d675f997dbf373cf436a81372817fe3de4be0
cd1 <cd1@socialthreesixty.co>
2025-03-12 10:40:26 +0000

    Changes in delete username route

--- src/routes/deleteUsername.ts
+++ src/routes/deleteUsername.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response } from "express";
-import avModel from "../model/avEmailModel";
+
 import usernameModel from "../model/UsernameModel";
 
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
   try {
-    const { subscriptionId, username } = req.json();
+    const { subscriptionId, username } = req.body;
     if (!subscriptionId || !username)
       return res.json({
         success: false,
